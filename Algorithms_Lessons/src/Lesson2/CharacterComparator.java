16fced9f273c3a4331582ef5afa169643e0b89f0
Kotuma Alexander <aikotuma@gmail.com>
2019-05-04 12:30:59 +0400

    Lesson2

---
+++ Algorithms_Lessons/src/Lesson2/CharacterComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+
+public class CharacterComparator implements Comparator<Character> {
+    @Override
+    public int compare(Character o1, Character o2) {
+        return o1.compareTo(o2);
+    }
+}
